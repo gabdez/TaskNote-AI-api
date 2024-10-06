829c860719798750c0a13d53579a89e47c914f2c
FERNANDEZ CASA Gabriel <gabriel.fernandezcasa@3ds.com>
2024-10-06 20:36:56 +0200

    install dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express()
 const port = process.env.PORT || 4000;
