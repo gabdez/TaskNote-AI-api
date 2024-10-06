faa94faa23ce9c4ceb70e5060955ebf59ea498b6
FERNANDEZ CASA Gabriel <gabriel.fernandezcasa@3ds.com>
2024-10-06 20:22:53 +0200

    connect to database

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express()
 const port = process.env.PORT || 4000;
 
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DB_URL;
+
+const uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
 app.get('/', (req, res) => {
+    run();
     res.send('Hello World!')
 })
 
@@ -10,6 +42,7 @@ app.get('/notes', (req, res) => {
     res.send(`TaskNote.ai API listening on port ${port} - jambutt`);
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await run();
     console.log(`TaskNote.ai API listening on port ${port}`)
 })
